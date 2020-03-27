427c12ef61db0eb4ac55a13981093b38c0c86658
liangzhifa <877657682@qq.com>
2020-03-27 15:15:45 +0800

    oracle下的分页查询

--- src/main/java/com/zhifa/mybatisoracle/controller/TestController.java
+++ src/main/java/com/zhifa/mybatisoracle/controller/TestController.java
@@ -1,12 +1,16 @@
 package com.zhifa.mybatisoracle.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.zhifa.mybatisoracle.domain.Lzf;
 import com.zhifa.mybatisoracle.mapper.LzfMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -25,5 +29,13 @@ public class TestController {
         lzfMapper.insert(lzf);
 
         return lzf;
+    }
+
+    @GetMapping("/list")
+    public PageInfo<Lzf> getList(){
+        PageHelper.startPage(1, 10);
+        List<Lzf> lzfList = lzfMapper.selectList(null);
+        PageInfo<Lzf> lzfPageInfo = new PageInfo<>(lzfList);
+        return lzfPageInfo;
     }
 }
